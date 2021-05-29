69e4bddfe2228fe3c35d0be4da44c13610969666
Commit two <info@splintinvestments.com>
2021-05-29 14:12:55 +0300

    Callback handler for Elia

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,4 +52,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'auth';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['callbacks/ellypay'] = "ellypay/handleCallback";
+$route['api/callbacks/ellypay'] = "ellypay/handleCallback";
