77bd30962d83b7f8b8d0cedfc4edca54e48502ba
Commit two <info@splintinvestments.com>
2021-05-29 14:51:05 +0300

    Theming modified

--- application/modules/auth/views/login.php
+++ application/modules/auth/views/login.php
@@ -93,7 +93,7 @@ $config= Modules::run("settings/getAll");
     <div id="primary" class="p-t-b-100 height-full">
         <div class="container">
             <div class="row">
-                <div class="col-lg-4 mx-md-auto paper-card" style="background-color: #20b3c7;">
+                <div class="col-lg-4 mx-md-auto paper-card" style="background-color: #075E54;">
                     <div class="text-center">
                         <img src="<?php echo base_url(); ?>assets/img/basic/<?php echo $config->main_logo; ?>" width="100px">
                         <h3 class="mt-2 text-white"><?php echo $config->system_name; ?></h3>
